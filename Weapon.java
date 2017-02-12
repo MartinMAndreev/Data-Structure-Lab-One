3dfb5b3041cd594d7a5f4179867597b7e77ba117
MartinMAndreev <martinandreev1223@gmail.com>
2017-02-12 16:34:33 -0500

    Completed Weapon -- With Comments

--- Weapon.java
+++ Weapon.java
@@ -1,18 +1,24 @@
 public class Weapon extends Item{
+  //the damage the weapon does in number form
   private int weaponDamage;
   
+  //method in order to find the weapons damage integer
   public int getWeaponDamage(){
     return weaponDamage;
   }
-  
+  //sets that to the variable damage
   public void setWeaponDamage(int damage){
     weaponDamage = damage;
   }
-  
+ 
+  /**
+   * Constructor to initialize the weaponDamage
+   * @param damage
+   */
   public Weapon(int damage){
     weaponDamage = damage;
   }
-  
+  //defult constructor
   public Weapon(){
   }
 }
