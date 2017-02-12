8fc1031ef2f6cc21af262c17218f1c9b1320c35a
MartinMAndreev <martinandreev1223@gmail.com>
2017-02-12 17:41:41 -0500

    Completed Food File now with comments

--- Food.java
+++ Food.java
@@ -1,8 +1,9 @@
 public class Food extends Item{
+  //the number for the nutrition and the quantity of the food
   private int foodNutrition;
   private int foodQuantity;
 
-  
+  //methods for getting and setting the nutrition and quantity of food
   public int getFoodNutrition(){
     return foodNutrition;
   }
@@ -14,15 +15,18 @@ public class Food extends Item{
   public int getFoodQuantity(){
     return foodQuantity;
   }
-  
+  //re directs it to newFoodQuantity
   public void setFoodQuantity(int newFoodQuantity){
     foodQuantity = newFoodQuantity;
   }
-  
+   /**
+   * Constructor to initialize the foodQuantity
+   * @param newFoodQuantity
+   */
   public Food(int newFoodQuantity){
     foodQuantity = newFoodQuantity;
   }
-  
+  //defult constructor
   public Food(){
   }
 }
